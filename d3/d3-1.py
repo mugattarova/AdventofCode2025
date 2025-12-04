b7f709694b41061e2f76e5dc846160754dd73fa8
mugattarova <mugattarova@gmail.com>
2025-12-04 21:24:02 +0200

    ✨Day 3 complete✨

---
+++ d3/d3-1.py
@@ -0,0 +1,12 @@
+batteries = []
+for line in open('input'):
+    line = list(line)
+    out = ''
+    mx, mxind = 0, 0
+    for i in range(len(line)-1,len(line)+1):
+        mx = max(line[mxind : i])
+        mxind = line.index(mx, mxind) + 1
+        out += mx
+    batteries.append(int(out))
+    
+print("Answer: "+str(sum(batteries)))
